058c0a48665e9d757465af514b634e2ecf09e8b2
Milos Vuksanovic <hex3solutions@gmail.com>
2021-02-10 16:11:42 +0100

    -refactor

---
+++ README.md
@@ -0,0 +1,33 @@
+# Code Challange - Create a fast finger application
+
+## Project description functionalities
+
+-The game begins with the appearance of letters of the word "POSAO"(eng translation - job)
+in the middle of screen, from where they animate towards random positions and stops with
+ease-out animation! (letters must not overlap).
+
+- The goal of the game is to player click within 5 seconds (desktop) or 2 seconds (mobile) device 
+all letters in the correct order.
+- If the order is wrong the game starts again, if player does click on all the words the game starts
+again.
+- If the user clicks on all the letters in the right order on time, the message pop up, "Bravo!" and button
+"Play again" which restarts the game.
+
+
+## Language
+
+- Javascript
+
+## Game description
+
+How fast can you click?
+Let's check out
+Good luck !
+
+### License
+
+Distributed under the MIT License.
+
+### Author
+
+Milos Vuksanovic
--- main.js
+++ main.js
@@ -1,147 +1,195 @@
-document.getElementById('startButton').addEventListener("click" , () => {
-    document.getElementById('startButton').classList.add("hide")
-    document.getElementById('countGameTime').classList.add("show")
-    newGame(document.getElementsByClassName('gameButtons'))
-})
-
-function openModal(gameButtons) {
-    document.getElementById('finishModal').style.display = "block"
-    document.getElementById('playAgain').addEventListener("click", () => {
-        newGame(gameButtons)
-        document.getElementById('finishModal').style.display = "none"
-    })
-}
-
-const game = {
-    state : 'Start-Game',
-    transitions: {
-        'Start-Game' : {
-            refresh : function refreshGame(startButtons) {
-
-            }
-        },
-        'Game-End' : {
-            successEnd: function successEnd() {
-
-            }
-        },
-
-    }
-}
-    
-    // let interval = setInterval(() => {
-    //     console.log(interval, 'inside interval')
-    //     timer += 1
-    //     document.getElementById('countGameTime').textContent = timer
-    //     if(isMobile() && timer === 3) {
-    //         clearInterval(interval)
-    //         refreshGame(gameButtons)
-    //     }
-    //     if(!isMobile() && timer === 10) {
-    //         console.log(interval, 'inside game over')
-    //         clearInterval(interval)
-    //         refreshGame(gameButtons)
-    //     }
-    // }, 1000);
-
-    // checkResult(gameButtons, userArray, resultArray, timer)
-// }
-
+document.getElementById("startButton").addEventListener("click", () => {
+  const startButton = document.getElementById("startButton")
+  const countTime = document.getElementById("countGameTime")
+  const gameButtons = document.querySelectorAll('.gameButtons')
+  
+  //show game time
+  countTime.classList.add("show")
+  // hide inital start
+  startButton.classList.add("hide");
+  // add events to buttons, once (no memory leak)
+  addEventsToButtons(gameButtons)
+  // init game
+  newGame(gameButtons);
+});
+// recursive function
 function newGame(gameButtons) {
-    const userArray = []
-    const resultArray = ["P","O","S","A","O"]
-    let positions = []
-    let allowedWidht = []
-    let allowedHeight = []
-    const canvasHeight = (( window.innerHeight ) / 2 ) - 36
-    const canvasWidth = (( window.innerWidth  ) / 2 ) - 36
-    const heightArray = createArrayOfNumbers( -canvasHeight, canvasHeight)
-    const widthArray = createArrayOfNumbers( -canvasWidth, canvasWidth)
-
-    allowedWidht.push(widthArray)
-    allowedHeight.push(heightArray)
-
-    for (let i = 0; i < gameButtons.length; i++) {
-        document.getElementById(`position-${i}`).classList.add("spread")
-        document.getElementById(`position-${i}`).classList.remove("mark-checked")
-
-        document.getElementById(`position-${i}`).addEventListener("click", () => {
-            document.getElementById(`position-${i}`).classList.add("mark-checked")
-
-            check(document.getElementById(`position-${i}`).innerHTML)
-        })
-        let randomPosition= [
-            randomArrayIndex(allowedWidht[i]),
-            randomArrayIndex(allowedHeight[i])
-        ]
-        positions.push(randomPosition)
-        allowedWidht.push(allowedPositions(positions[i][0], allowedWidht[i]))
-        allowedHeight.push(allowedPositions(positions[i][1], allowedHeight[i]))
-        gameButtons[i].style.transform =`translate(${ positions[i][0] }px,${ positions[i][1] }px)`; 
-    }
-
+  // init state
+  initalState()
+  // random position buttons
+  schuffleButtons(gameButtons)
+  // start timer
+  let timer = startTimer()
+  // time runs out, refresh game or stop if won
+  Promise.resolve().then(
+    setTimeout(() => {
+      clearInterval(timer)
+      let { state, length } = validateState()
+      // if player won - stop timer, dont refresh
+      if((!state && !length) || (state && !length)) {
+        removeCheckedButtons(gameButtons)
+        newGame(gameButtons)
+      }
+    },deviceTimer())
+  )
 }
+// spread buttons to random positions
+function schuffleButtons(gameButtons) {
+  let allowedWidht = [];
+  let allowedHeight = [];
+  let positions = [];
+  // canvas by pixels minus size of button to edge's
+  const canvasHeight = window.innerHeight / 2 - 40;
+  const canvasWidth = window.innerWidth / 2 - 40;
+  // create arrays x,y positions
+  const heightArray = createArrayOfNumbers(-canvasHeight, canvasHeight);
+  const widthArray = createArrayOfNumbers(-canvasWidth, canvasWidth);
 
+  // inital position 
+  allowedWidht.push(widthArray);
+  allowedHeight.push(heightArray);
 
-// function checkResult(gameButtons) {
-//     for (let i = 0; i < gameButtons.length; i++) {
-//         document.getElementById(`position-${i}`).addEventListener("click", () => {
-//             document.getElementById(`position-${i}`).classList.add("mark-checked")
-//         })
-        //     userArray.push(document.getElementById(`position-${i}`).innerHTML)
-        //     const result = compareArrays(userArray, resultArray)
-        //     const lenght = compareLenght(userArray, resultArray)
-        //     console.log(result, 'result')
-        //     if(result && lenght) {
-        //         openModal(gameButtons)
-        //     }
-        //     if(!result) {
-        //         refreshGame(gameButtons)
-        //     }
-        // })
-    // }
-// }
-function compareLenght(userArray, resultArray) {
-    return userArray.length === resultArray.length ? true : false
+  // loop tru gamebuttons, assign random position
+  gameButtons.forEach((button , i) => {
+    // get random index of x,y position array
+    const randomPosition = [
+      randomArrayIndex(allowedWidht[i]),
+      randomArrayIndex(allowedHeight[i]),
+    ];
+    positions.push(randomPosition);
+    // exclude declared x,y positions
+    allowedWidht.push(allowedPositions(positions[i][0], allowedWidht[i]));
+    allowedHeight.push(allowedPositions(positions[i][1], allowedHeight[i]));
+    // add to button style
+    button.style.transform = `translate(${positions[i][0]}px,${positions[i][1]}px)`;
+  })
 }
-
-function compareArrays(userArray, resultArray) {
-    const index = userArray.length
-    resultArray = resultArray.slice(0, index)
-    return JSON.stringify(userArray) === JSON.stringify(resultArray) ? true : false
-}
-
-function getRandomNumber(min, max) {
-    const define = max - min + 1
-    const generate = Math.random() * define
-    return Math.floor(generate) + min
-}
-
-function createArrayOfNumbers(start, end) {
-    let arr = []
-    for (let i = start; i <= end; i++) {
-        arr.push(i)
-    }
-    return arr
-}
-
-function randomArrayIndex(array) {
-    let index = getRandomNumber(0, array.length)
-    return array[index]
-}
-
-function allowedPositions(value, array) {
-    let notAllowedPositions = []
-    for (let i = - 18; i <= 18; i++) {
-        notAllowedPositions.push(value + i)
-    }
-    array = array.filter(( el ) => {
-        return notAllowedPositions.indexOf( el ) < 0
+// eventListener on buttons
+function addEventsToButtons(gameButtons) {
+  gameButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      // add button value to state on click
+      addButtonToState(button)
+      // validate state 
+      let { state, length } = validateState()
+      // call new game
+      if(!state) {
+        removeCheckedButtons(gameButtons)
+        newGame(gameButtons)
+      }
+      if(state && length) {
+        openModal(gameButtons)
+      }
     })
-    return array
+  })
+}
+// mark checked buttons, update state
+function addButtonToState (_button) {
+  const button = _button.innerHTML
+  _button.classList.add('mark-checked')
+  updateState(button)
+}
+// open finished game modal
+function openModal(gameButtons) {
+    const finishModal = document.getElementById('finishModal')
+    const playAgain = document.getElementById('playAgain')
+    finishModal.style.display = "block"
+    playAgain.addEventListener("click", () => {
+        removeCheckedButtons(gameButtons)
+        newGame(gameButtons)
+        finishModal.style.display = "none"
+    })
+}
+// remove class from button
+function removeCheckedButtons(gameButtons) {
+  gameButtons.forEach(button => button.classList.remove('mark-checked'))
+}
+// init state
+function initalState() {
+  let state = []
+  localStorage.setItem('gameState', JSON.stringify(state))
+}
+// get state
+function getState() {
+  return JSON.parse(localStorage.getItem("gameState"));
 }
 
-function isMobile() {
-    return window.innerWidth < 480 ? true : false
+// update state
+function updateState(button) {
+  const userArray = getState()
+  userArray.push(button);
+  localStorage.setItem('gameState', JSON.stringify(userArray))
+}
+
+// returns boolean, compare lenght of 2 arr
+function compareLenght(userArray, resultArray) {
+  return userArray.length === resultArray.length ? true : false
+}
+
+
+// returns iterval id, show timer on screen and start executing every sec
+function startTimer() {
+  let countGameTime = document.getElementById('countGameTime')
+  let timer = 1
+  countGameTime.textContent = timer
+  let interval = setInterval(() => {
+    timer += 1
+    // show timer 
+    countGameTime.textContent = timer
+    // increment timer by 1000ms
+  }, 1000);
+  return interval
+}
+
+// returns boolean, compare user clicked buttons and final result
+function validateState() {
+  const resultArray = ["P", "O", "S", "A", "O"];
+  const userArray = getState()
+  const length = compareLenght(userArray, resultArray)
+  const state = compareArrays(userArray, resultArray)
+  return { state, length }
+}
+
+// returns boolean , compare 2 arr
+function compareArrays(userArray, resultArray) {
+  const index = userArray.length
+  resultArray = resultArray.slice(0, index)
+  return JSON.stringify(userArray) === JSON.stringify(resultArray)
+    ? true
+    : false
+}
+// returns genereted random number
+function getRandomNumber(min, max) {
+  const define = max - min + 1
+  const generate = Math.random() * define
+  return Math.floor(generate) + min
+}
+// returns array of numbers
+function createArrayOfNumbers(start, end) {
+  let arr = []
+  for (let i = start; i <= end; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+// returns random index in array
+function randomArrayIndex(array) {
+  let index = getRandomNumber(0, array.length)
+  return array[index]
+}
+// returns Array with excluded position(size of button)
+function allowedPositions(value, array) {
+  let notAllowedPositions = []
+  for (let i = -20; i <= 20; i++) {
+    notAllowedPositions.push(value + i)
+  }
+  array = array.filter((el) => {
+    return notAllowedPositions.indexOf(el) < 0
+  });
+  return array;
+}
+// returns seconds for diffrent screen sizes
+function deviceTimer() {
+  return window.innerWidth < 480 ? 2000 : 5000
 }
 
